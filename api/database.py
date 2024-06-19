c074076531f2917335f8f18df357da1f2d5c2822
hendrik127 <hendrik.parik@gmail.com>
2024-06-19 10:36:48 +0300

    adding example env file

--- api/database.py
+++ api/database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', 'example.env'))
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
--- api/fetch_data.py
+++ api/fetch_data.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', 'example.env'))
 
 
 def fetch_iris_data() -> Optional[str]:
